2392e28297c5b2089c4f64c8e770df66a97fb808
Unknown <arseniyzhuck@mail.ru>
2020-05-01 13:54:34 +0300

    1_6 DZ

--- prog1_6/src/Loader.java
+++ prog1_6/src/Loader.java
@@ -5,7 +5,8 @@ import java.util.Date;
 public class Loader {
 
     public static void main(String[] args) {
-        DateFormat df= new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        DateFormat df= new SimpleDateFormat("HH:mm dd.MM.yyyy");
+        //DateFormat df= new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date d= new Date();
         System.out.println(df.format(d));
     }
