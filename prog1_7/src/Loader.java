1a51c380d91ba812ceea074a1af23e36dc4c5b44
Unknown <arseniyzhuck@mail.ru>
2020-05-01 14:03:02 +0300

    1_7 lesson

--- prog1_7/src/Loader.java
+++ prog1_7/src/Loader.java
@@ -1,2 +1,17 @@
-public class Loader {
+import javax.swing.*;
+import java.awt.*;
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+        JFrame main_window =new JFrame();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        main_window.setSize(dim);
+        main_window.setTitle("Hello world:  IT MUST BE HERE");
+        main_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        main_window.setVisible(true);
+
+
+    }
 }
