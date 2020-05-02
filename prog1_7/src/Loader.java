a04281792ea7e7f862c515634ad36541b8fead60
Unknown <arseniyzhuck@mail.ru>
2020-05-02 13:13:10 +0300

    1_7_Ispravlenno

--- prog1_7/src/Loader.java
+++ prog1_7/src/Loader.java
@@ -6,13 +6,14 @@ public class Loader
     public static void main(String[] args)
     {
         JFrame main_window =new JFrame();
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-        int posX= dim.width/2-400;
-        int posY= dim.height/2-300;
-        
-        main_window.setLocation(posX,posY);
+        //Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        //int posX= dim.width/2-400;
+        //int posY= dim.height/2-300;
+        //main_window.pack();
+        //main_window.setLocation(posX,posY);
         //main_window.setSize(dim);
         main_window.setSize(800, 600);
+        main_window.setLocationRelativeTo(null);
         main_window.setTitle("Hello world:  IT MUST BE HERE");
         main_window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         main_window.setVisible(true);
